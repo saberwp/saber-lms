6970733932f3143b35fe61352f572b500172b957
Casey Milne <casey@saberwp.com>
2023-03-03 02:43:22 -0600

    Single question template, doc draft.

---
+++ docs/questions.md
@@ -0,0 +1,5 @@
+# Questions
+
+Questions are a post type with ACF field group that provides a repeater field "answer_list".
+
+There is a single post template that renders the question with answers in an unordered list.
--- templates/single-question.php
+++ templates/single-question.php
@@ -1,12 +1,26 @@
-<?php get_header(); ?>
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
-<div class="bg-green-200 h-40 p-8">
-	<?php
+<?php get_header('app'); ?>
 
-		$answers = get_field('answer_list');
-		var_dump( $answers );
+<div class="p-4 max-w-2xl mx-auto">
+	<h1 class="font-semibold text-2xl"><?php the_title(); ?></h1>
+	<?php the_content(); ?>
+	<div class="bg-green-200 h-40 p-8">
+		<?php
 
-	?>
+			$answers = get_field('answer_list');
+
+			if( ! empty( $answers )) {
+				echo '<ul>';
+				foreach( $answers as $answer ) {
+					echo '<li class="cursor-pointer font-medium text-lg hover:bg-green-800 hover:text-white">';
+					echo $answer['answer'];
+					echo '</li>';
+				}
+				echo '</ul>';
+			}
+
+		?>
+		<button class="font-semibold text-white bg-gray-800 rounded-md px-4 py-2">ANSWER</button>
+	</div>
 </div>
-<?php get_footer(); ?>
+
+<?php get_footer('app'); ?>
