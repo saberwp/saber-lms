805bdd02ab94ac7da53ce7bbb8e2822bc98d652b
Casey Milne <casey@saberwp.com>
2023-03-09 16:30:19 -0600

    Styles for answer screen.

--- templates/parts/quiz-screen-answer.php
+++ templates/parts/quiz-screen-answer.php
@@ -1,21 +1,23 @@
 <template id="quiz-screen-answer-template">
 
-	<div class="p-4 max-w-2xl mx-auto">
-		<h1 id="question-text" class="font-semibold text-2xl"></h1>
-		<div class="bg-green-200 h-40 p-8">
+	<div class="max-w-2xl mx-auto">
+		<h1 id="question-text" class="font-semibold text-2xl mb-8"></h1>
+		<div class="bg-emerald-100 p-8 flex flex-col gap-4">
 			<ul id="question-answer-list" class="answer-list"></ul>
-			<button class="slms-answer-question font-semibold text-white bg-gray-800 rounded-md px-4 py-2">ANSWER</button>
 		</div>
+		<button id="quiz-answer-button" class="slms-answer-button block my-4 w-40 bg-emerald-800 text-white rounded-md py-3 px-8 font-medium text-lg hover:bg-emerald-700">
+			ANSWER
+		</button>
 	</div>
 
-	<div>
-		<button id="quiz-previous-button">
+	<div class="my-8 flex gap-4">
+		<button id="quiz-previous-button" class="w-32 bg-gray-800 text-white rounded-md py-2 px-6 font-medium hover:bg-gray-700">
 			<span>PREVIOUS</span>
 		</button>
-		<button id="quiz-next-button">
+		<button id="quiz-next-button" class="w-32 bg-gray-800 text-white rounded-md py-2 px-6 font-medium hover:bg-gray-700">
 			<span>NEXT</span>
 		</button>
-		<button id="quiz-review-button">
+		<button id="quiz-review-button" class="w-32 bg-gray-800 text-white rounded-md py-2 px-6 font-medium hover:bg-gray-700">
 			<span>REVIEW</span>
 		</button>
 	</div>
