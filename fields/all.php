304dd507305a8d7d1e4829097b6b3af9274a3650
Casey Milne <casey@saberwp.com>
2023-03-09 18:02:00 -0600

    Update field exports.

--- fields/all.php
+++ fields/all.php
@@ -4,7 +4,7 @@ if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
 'key' => 'group_63fa8cedcf71f',
-'title' => 'Question Answers',
+'title' => 'Saber LMS > Questions > Question Answers',
 'fields' => array(
 array(
 'key' => 'field_63fa8cee06ac2',
@@ -91,6 +91,57 @@ array(
 'active' => true,
 'description' => '',
 'show_in_rest' => 1,
+));
+
+acf_add_local_field_group(array(
+'key' => 'group_640a305f5a9c5',
+'title' => 'Saber LMS > Quiz > Question List',
+'fields' => array(
+array(
+'key' => 'field_640a30d7cd7f4',
+'label' => 'Question List',
+'name' => 'question_list',
+'aria-label' => '',
+'type' => 'relationship',
+'instructions' => '',
+'required' => 1,
+'conditional_logic' => 0,
+'wrapper' => array(
+	'width' => '',
+	'class' => '',
+	'id' => '',
+),
+'post_type' => array(
+	0 => 'question',
+),
+'taxonomy' => '',
+'filters' => array(
+	0 => 'search',
+),
+'return_format' => 'id',
+'min' => 1,
+'max' => 25,
+'elements' => '',
+),
+),
+'location' => array(
+array(
+array(
+	'param' => 'post_type',
+	'operator' => '==',
+	'value' => 'quiz',
+),
+),
+),
+'menu_order' => 0,
+'position' => 'normal',
+'style' => 'default',
+'label_placement' => 'top',
+'instruction_placement' => 'label',
+'hide_on_screen' => '',
+'active' => true,
+'description' => '',
+'show_in_rest' => 0,
 ));
 
 endif;		
